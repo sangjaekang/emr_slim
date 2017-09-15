d8d295d3cc6639ca247fc847d471f402aa225546
sangjaekang <sangjaekang48@gmail.com>
2017-09-15 12:03:35 +0900

    mean min max 매트릭스　중첩된　numpy　생성

--- generate_labtest.py
+++ generate_labtest.py
@@ -11,7 +11,7 @@ os_path = os.path.abspath('./') ; find_path = re.compile('emr_slim')
 BASE_PATH = os_path[:find_path.search(os_path).span()[1]]
 sys.path.append(BASE_PATH)
 
-from construct_labtest import get_labtest_df
+from construct_labtest import get_labtest_df, get_labtest_aggregated_df
 
 DEBUG_PRINT=True
 
@@ -25,12 +25,12 @@ PREP_OUTPUT_DIR  = DATA_DIR + 'prep/'
 INPUT_DIR = DATA_DIR +'input/'
 LABEL_PATIENT_PATH = 'label_patient_df.h5'
 
-def save_patient_input(no_range,label_name,save_dir=None,time_length=6,gap_length=1,target_length=3,offset_min_counts=50,offset_max_counts=100):
+def save_patient_input(no_range,label_name,aug_target=None,save_dir=None,time_length=6,gap_length=1,target_length=3,offset_min_counts=50,offset_max_counts=100):
     global DEBUG_PRINT, PREP_OUTPUT_DIR, LABEL_PATIENT_PATH
     if save_dir is None:
         save_dir = INPUT_DIR
 
-    for label_value in range(0,4):
+    for label_value in range(0,3):
         o_path = save_dir +'{}/'.format(label_value)
         if not os.path.isdir(o_path):
             os.makedirs(o_path)
@@ -38,12 +38,18 @@ def save_patient_input(no_range,label_name,save_dir=None,time_length=6,gap_lengt
     # syntax checking existence for directory
     PREP_OUTPUT_DIR = check_directory(PREP_OUTPUT_DIR)
     output_path = PREP_OUTPUT_DIR + LABEL_PATIENT_PATH
-    label_df = pd.HDFStore(output_path,mode='r').select('label/{}'.format(label_name))
+    label_store = pd.HDFStore(output_path,mode='r')
+    label_df = label_store.select('label/{}'.format(label_name))
+    label_store.close()
 
     colist = get_timeseries_column()
 
+    if aug_target is not None:
+        #라벨의　비대칭성을　해소하기위해　특정　라벨을　포함한　것을　위주로　no_range를　추려냄
+        no_range = set(no_range)&set(label_df[label_df.label == aug_target].no.values)
+        
     for idx, no in enumerate(no_range):
-        if DEBUG_PRINT and idx %50 == 0:  print("process({}){} th start".format(os.getpid(),idx))
+        if DEBUG_PRINT and idx %1000 == 0:  print("process({}){} th start".format(os.getpid(),idx))
         emr_df = get_labtest_df(no)
         label_series = get_patient_timeseries_label(no,label_df)
         if emr_df.count().sum() <= offset_min_counts : continue
@@ -58,12 +64,53 @@ def save_patient_input(no_range,label_name,save_dir=None,time_length=6,gap_lengt
                 np.save(file_path,window.as_matrix())
 
 
+def save_patient_mean_min_max(no_range,label_name,aug_target=None,save_dir=None,time_length=6,gap_length=1,target_length=3,offset_min_counts=50,offset_max_counts=100):
+    global DEBUG_PRINT, PREP_OUTPUT_DIR, LABEL_PATIENT_PATH
+    if save_dir is None:
+        save_dir = INPUT_DIR
+
+    for label_value in range(0,3):
+        o_path = save_dir +'{}/'.format(label_value)
+        if not os.path.isdir(o_path):
+            os.makedirs(o_path)
+
+    # syntax checking existence for directory
+    PREP_OUTPUT_DIR = check_directory(PREP_OUTPUT_DIR)
+    output_path = PREP_OUTPUT_DIR + LABEL_PATIENT_PATH
+    label_store = pd.HDFStore(output_path,mode='r')
+    label_df = label_store.select('label/{}'.format(label_name))
+    label_store.close()
+    colist = get_timeseries_column()
+
+    if aug_target is not None:
+        #라벨의　비대칭성을　해소하기위해　특정　라벨을　포함한　것을　위주로　no_range를　추려냄
+        no_range = set(no_range)&set(label_df[label_df.label == aug_target].no.values)
+        
+    for idx, no in enumerate(no_range):
+        if DEBUG_PRINT and idx %1000 == 0:  print("process({}){} th start".format(os.getpid(),idx))
+        mean_df,min_df,max_df = get_labtest_aggregated_df(no)
+        label_series = get_patient_timeseries_label(no,label_df)
+
+        if mean_df.count().sum() <= offset_min_counts : continue
+        for i in range(0,len(colist) - (time_length+gap_length+target_length)):
+            window = mean_df.loc[:,colist[i]:colist[i+time_length-1]]
+            counts_in_window = window.count().sum()
+            if counts_in_window >= offset_min_counts and counts_in_window <= offset_max_counts:
+                target_stime = colist[time_length-1+gap_length+i]
+                label_value = check_label(label_series.loc[target_stime:get_add_interval(target_stime,target_length-1)])
+                if np.isnan(label_value): continue
+                file_path = save_dir +'{}/{}_{}.npy'.format(label_value,no,i)
+                avg_mat = window.as_matrix()
+                min_mat = min_df.loc[:,colist[i]:colist[i+time_length-1]].as_matrix()
+                max_mat = max_df.loc[:,colist[i]:colist[i+time_length-1]].as_matrix()
+                np.save(file_path,np.stack((avg_mat,min_mat,max_mat)))
+
+
 def get_patient_timeseries_label(no,label_df):
     global  PREP_OUTPUT_DIR,  LABEL_PATIENT_PATH
     result_series = pd.Series(index=get_timeseries_column())    
     for _,label_df_date,label_df_label in label_df[label_df.no==no].values:
-        result_series[label_df_date]=label_df_label
-    
+        result_series[label_df_date]=label_df_label    
     return result_series
 
 
@@ -79,7 +126,7 @@ def check_directory(directory_path):
 
 
 def yield_time_column(time,end_t):
-    # time-series column을　만드는데　있어서　순차적으로　생성하는　함수X
+    # time-series column을　만드는데　있어서　순차적으로　생성하는　함수
     # 1210 1211 1212 1301 1302 이런식으로　생성
     while time <= end_t:
         yield time
@@ -146,7 +193,6 @@ def write_metadata_README(path, label_name,time_length,gap_length,target_length,
     with open(file_path,'w') as f:
         f.write(metadata_README)
 
-
 def check_label(x):
     NORMAL_FLAG = False
     for _,y in x.items():
@@ -160,11 +206,11 @@ def check_label(x):
     else:
         return np.nan
 
-
 def _set_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('path', help='save path')
     parser.add_argument('label', help='label_name')
+    parser.add_argument('set_type', help='train test validation')
     parser.add_argument('chunk_size', help='the number of patients using per one process')
     parser.add_argument('time_length',help='time_length')
     parser.add_argument('gap_length', help='gap_length')
@@ -174,11 +220,11 @@ def _set_parser():
     args = parser.parse_args()
     return args
 
-
 if __name__ == '__main__':
     #argument
     args = _set_parser()
     label_name = args.label 
+    set_type = args.set_type
     chunk_size = int(args.chunk_size)
     time_length = int(args.time_length) 
     gap_length = int(args.gap_length)
@@ -187,54 +233,26 @@ if __name__ == '__main__':
     offset_max_counts = int(args.offset_max_counts) 
     
     o_path = check_directory(args.path)
-
-    train_path = o_path + 'train/'; train_path = check_directory(train_path)
-    test_path = o_path + 'test/'; test_path = check_directory(test_path)
-    validation_path = o_path + 'validation/'; validation_path = check_directory(validation_path)
-
+    data_path = o_path + set_type
+    data_path = check_directory(data_path)
+    write_metadata_README(o_path, label_name,time_length,gap_length,target_length,offset_min_counts,offset_max_counts)
+    
     PREP_OUTPUT_DIR = check_directory(PREP_OUTPUT_DIR)
     output_path = PREP_OUTPUT_DIR + LABEL_PATIENT_PATH
 
-    write_metadata_README(o_path, label_name,time_length,gap_length,target_length,offset_min_counts,offset_max_counts)
-
     sample_store = pd.HDFStore(output_path,mode='r')
-    train_set = sample_store.select('label/{}/train'.format(label_name)).no.unique()
-    validation_set = sample_store.select('label/{}/validation'.format(label_name)).no.unique()
-    test_set = sample_store.select('label/{}/test'.format(label_name)).no.unique()
+    data_set = sample_store.select('label/{}/{}'.format(label_name,set_type)).no.unique()
     sample_store.close()
 
-    ## train set generating
+    ## data_set generating
     print("Creating pool with 8 workers")
     start_time = time.time()
+
     pool = multiprocessing.Pool(processes=8)
-    print("Invoking apply train_set")
-    for divider in np.array_split(train_set,8):
-        pool.apply_async(save_patient_input,[divider[:chunk_size],label_name,train_path,time_length,
+    print("Invoking apply {}_set".format(set_type))
+    for divider in np.array_split(data_set,8):
+        pool.apply_async(save_patient_input,[divider[:chunk_size],label_name,data_path,time_length,
                      gap_length,target_length,offset_min_counts,offset_max_counts])
     pool.close()
     pool.join()    
-    print("trainset Finished--consumed time : {}".format(time.time()-start_time))
-
-    ## test set generating
-    print("Creating pool with 8 workers")
-    start_time = time.time()
-    pool = multiprocessing.Pool(processes=8)
-    print("Invoking apply test_set")
-    for divider in np.array_split(test_set,8):
-        pool.apply_async(save_patient_input,[divider[:4000],label_name,test_path,time_length,
-                     gap_length,target_length,offset_min_counts,offset_max_counts])
-    pool.close()
-    pool.join()    
-    print("testset Finished--consumed time : {}".format(time.time()-start_time))
-
-    ## validation set generating
-    print("Creating pool with 8 workers")
-    start_time = time.time()
-    pool = multiprocessing.Pool(processes=8)
-    print("Invoking apply test_set")
-    for divider in np.array_split(validation_set,8):
-        pool.apply_async(save_patient_input,[divider[:4000],label_name,validation_path,time_length,
-                     gap_length,target_length,offset_min_counts,offset_max_counts,])
-    pool.close()
-    pool.join()    
-    print("validation Finished--consumed time : {}".format(time.time()-start_time))
+    print("{}_set Finished--consumed time : {}".format(set_type, time.time()-start_time))
